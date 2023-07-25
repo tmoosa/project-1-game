06afdb7b47d3bc1d19b4bafba0d78c74ea38c930
Taher Moosa <lilt@Tahers-MacBook-Pro.local>
2023-07-25 14:39:59 -0500

    Tic tac toe css & Javascript

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ const winningConditions = [
 [0, 4, 8],
 [2, 4, 6]
 ];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}  
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+    const winCondition = winningConditions[i];
+    const a = gameState[winCondition[0]];
+    const b = gameState[winCondition[1]];
+    const c = gameState[winCondition[2]];
+    if (a === "" || b === "" || c === "") continue;
+    if (a === b && b === c) {
+        roundWon = true;
+        break;
+    }
+    }

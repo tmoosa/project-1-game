4d6945593865079147cd7a86d3e53dc9639f76fc
Taher Moosa <lilt@Tahers-MacBook-Pro.local>
2023-07-25 11:56:44 -0500

    Javascript table

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//Game 
+const statusDisplay = document.querySelector(".game-status");
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+const winningConditions = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]
+];

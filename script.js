aaecd205f0cf1996c25516fe56fd76b55c380d62
Taher Moosa <lilt@Tahers-MacBook-Pro.local>
2023-07-27 13:43:04 -0500

    Finished Javascript code

--- script.js
+++ script.js
@@ -22,40 +22,40 @@ const winningConditions = [
 ];
 
 function handleCellPlayed(clickedCell, clickedCellIndex) {
-    gameState[clickedCellIndex] = currentPlayer;
-    clickedCell.innerHTML = currentPlayer;
+gameState[clickedCellIndex] = currentPlayer;
+clickedCell.innerHTML = currentPlayer;
 }  
 function handlePlayerChange() {
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = currentPlayerTurn();
+currentPlayer = currentPlayer === "X" ? "O" : "X";
+statusDisplay.innerHTML = currentPlayerTurn();
 }
 
 function handleResultValidation() {
-    let roundWon = false;
-    for (let i = 0; i <= 7; i++) {
+let roundWon = false;
+for (let i = 0; i <= 7; i++) {
     const winCondition = winningConditions[i];
     const a = gameState[winCondition[0]];
     const b = gameState[winCondition[1]];
     const c = gameState[winCondition[2]];
-    if (a === "" || b === "" || c === "") continue;
-    if (a === b && b === c) {
-        roundWon = true;
-        break;
+if (a === "" || b === "" || c === "") continue;
+if (a === b && b === c) {
+roundWon = true;
+break;
     }
     }
 
 
 if (roundWon) {
-    statusDisplay.innerHTML = winningMessage();
-    gameActive = false;
-    return;
+statusDisplay.innerHTML = winningMessage();
+gameActive = false;
+return;
 }
 
 const roundDraw = !gameState.includes("");
 if (roundDraw) {
-    statusDisplay.innerHTML = drawMessage();
-    gameActive = false;
-    return;
+statusDisplay.innerHTML = drawMessage();
+gameActive = false;
+return;
 }
 
 handlePlayerChange();
@@ -85,3 +85,44 @@ document
 document
 .querySelector(".game-restart")
 .addEventListener("click", handleRestartGame);
+
+function generateMultiplicationQuestion() {
+const num1 = Math.floor(Math.random() * 10) + 1;
+const num2 = Math.floor(Math.random() * 10) + 1;
+return `${num1} * ${num2}`;
+}
+
+function validateMultiplicationAnswer(answer, question) {
+const [num1, num2] = question.split(" * ");
+return parseInt(answer) === parseInt(num1) * parseInt(num2);
+}
+
+function askMultiplicationQuestion() {
+    const question = generateMultiplicationQuestion();
+    const userAnswer = prompt(
+    `Before you play your turn, answer this: ${question}`
+    );
+    if (
+    userAnswer === null ||
+    userAnswer.trim() === "" ||
+    !validateMultiplicationAnswer(userAnswer, question)
+    ) {
+    alert("Incorrect answer! Your turn is skipped.");
+    return false;
+    }
+    return true;
+}
+
+function handleCellClick(clickedCellEvent) {
+const clickedCell = clickedCellEvent.target;
+const clickedCellIndex = parseInt(clickedCell.getAttribute("data-box-index"));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) return;
+
+    if (askMultiplicationQuestion()) {
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+    } else {
+    handlePlayerChange();
+    }
+}
